19905734999b7db274593204253b3a101d1a0483
elhamwidiyanto <elhamwidiyanto@gmail.com>
2024-11-18 14:12:24 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//route home
+Route::get('/', function () {
+    return \Inertia\Inertia::render('Auth/Login');
+})->middleware('guest');
+
+//prefix "apps"
+Route::prefix('apps')->group(function() {
+
+    //middleware "auth"
+    Route::group(['middleware' => ['auth']], function () {
+
+        //route dashboard
+        Route::get('dashboard', App\Http\Controllers\Apps\DashboardController::class)->name('apps.dashboard');
+    
+    });
+});
